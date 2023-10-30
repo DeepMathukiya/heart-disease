6f40d0ae6c500d5a2bb646af90905bcd683c1133
DeepMathukiya <dsmathukiya2004@gmail.com>
2023-10-30 12:06:18 +0530

    final2

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Python\ML by Dr.DS sir\heart-disease dataset\venv
+set VIRTUAL_ENV=D:\Python\ML by Dr.DS sir\heart-disease\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
